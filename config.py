75c2dea4b67c7d71612adcc91f9f7418ecb4788f
CrouseMatthew <mcrouse@gmail.com>
2017-04-02 16:41:28 -0700

    Can't connect to PSQL database

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    # database URL
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/group-to-do"
+    # having a hard time getting evironment variables to work
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
